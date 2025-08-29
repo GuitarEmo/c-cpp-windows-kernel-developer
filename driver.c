c5fdcf56d69990f4af3f031b7fff3ba01aa09e7a
Nikita <nikita.pyanzin@bk.ru>
2025-08-29 14:07:34 +0300

    Create driver.c

---
+++ driver.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <windows.h>
+
+void callToAction() {
+    printf("Откликайся скорее!\n");
+}
+
+int main() {
+    printf("This is a placeholder for Windows driver code.\n");
+    callToAction();
+    printf("Не пропусти возможность!\n");
+    callToAction();
+    return 0;
+}
